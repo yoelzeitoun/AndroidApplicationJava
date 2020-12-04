eb860d2499a56cec80c81590282add0a2f1b0d2b
yoelzeitoun <yoelzeit@gmail.com>
2020-12-04 12:47:09 +0200

    added if to check

--- app/src/main/java/il/co/expertize/androidapplicationjava/UI/AddTravelActivity.java
+++ app/src/main/java/il/co/expertize/androidapplicationjava/UI/AddTravelActivity.java
@@ -19,24 +19,31 @@ public class AddTravelActivity extends AppCompatActivity {
     EditText name;
     EditText email;
     private TravelViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_travel_activity);
 
-        button = (Button)findViewById(R.id.button);
-        name = (EditText)findViewById(R.id.name);
-        email = (EditText)findViewById(R.id.email);
+        button = (Button) findViewById(R.id.button);
+        name = (EditText) findViewById(R.id.name);
+        name.clearFocus();
+        email = (EditText) findViewById(R.id.email);
         viewModel = new ViewModelProvider(this).get(TravelViewModel.class);
     }
 
-    public void SendRequest(View view)
-    {
-        Travel travel = new Travel();
-        travel.setClientName(name.getText().toString());
-        travel.setClientEmail(email.getText().toString());
-        if (viewModel.insertTravel(travel))
-            Toast.makeText(this, "TOP", Toast.LENGTH_LONG).show();;
-    }
+    public void SendRequest(View view) {
 
+        if (name.getText().toString().isEmpty() || email.getText().toString().isEmpty())
+            Toast.makeText(this, "Please Fill All the Details", Toast.LENGTH_LONG).show();
+        else {
+            Travel travel = new Travel();
+            travel.setClientName(name.getText().toString());
+            name.setText("");
+            travel.setClientEmail(email.getText().toString());
+            email.setText("");
+            if (viewModel.insertTravel(travel))
+                Toast.makeText(this, "TOP", Toast.LENGTH_LONG).show();
+        }
+    }
 }
