4d6aa3ddc1348eecfe930436b11590bac991beac
pinhas meguideche <pinoubg@live.fr>
2020-12-05 21:58:38 +0200

    color Toast

--- app/src/main/java/il/co/expertize/androidapplicationjava/Models/Travel.java
+++ app/src/main/java/il/co/expertize/androidapplicationjava/Models/Travel.java
@@ -1,5 +1,11 @@
 package il.co.expertize.androidapplicationjava.Models;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Travel {
 
     private String travelId = "id";
@@ -35,6 +41,119 @@ public class Travel {
 
     public void setClientEmail(String clientEmail) {
         this.clientEmail = clientEmail;
+    }
+
+
+    @TypeConverters(UserLocationConverter.class)
+    private UserLocation travelLocation;
+
+    @TypeConverters(RequestType.class)
+    private RequestType requesType;
+
+    @TypeConverters(DateConverter.class)
+    private Date travelDate;
+
+    @TypeConverters(DateConverter.class)
+    private Date arrivalDate;
+
+
+    private HashMap<String, Boolean> company;
+
+
+
+    public Travel() {
+    }
+
+
+
+
+
+    public static class DateConverter {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        @TypeConverter
+        public Date fromTimestamp(String date) throws ParseException {
+            return (date == null ? null : format.parse(date));
+        }
+
+        @TypeConverter
+        public String dateToTimestamp(Date date) {
+            return date == null ? null : format.format(date);
+        }
+    }
+
+
+
+    public enum RequestType {
+        sent(0), accepted(1), run(2), close(3);
+        private final Integer code;
+        RequestType(Integer value) {
+            this.code = value;
+        }
+        public Integer getCode() {
+            return code;
+        }
+        @TypeConverter
+        public static RequestType getType(Integer numeral) {
+            for (RequestType ds : values())
+                if (ds.code.equals(numeral))
+                    return ds;
+            return null;
+        }
+        @TypeConverter
+        public static Integer getTypeInt(RequestType requestType) {
+            if (requestType != null)
+                return requestType.code;
+            return null;
+        }
+    }
+
+
+
+
+    public static class CompanyConverter {
+        @TypeConverter
+        public HashMap<String, Boolean> fromString(String value) {
+            if (value == null || value.isEmpty())
+                return null;
+            String[] mapString = value.split(","); //split map into array of (string,boolean) strings
+            HashMap<String, Boolean> hashMap = new HashMap<>();
+            for (String s1 : mapString) //for all (string,boolean) in the map string
+            {
+                if (!s1.isEmpty()) {//is empty maybe will needed because the last char in the string is ","
+                    String[] s2 = s1.split(":"); //split (string,boolean) to company string and boolean string.
+                    Boolean aBoolean = Boolean.parseBoolean(s2[1]);
+                    hashMap.put(/*company string:*/s2[0], aBoolean);
+                }
+            }
+            return hashMap;
+        }
+
+        //@TypeConverter
+        public String asString(HashMap<String, Boolean> map) {
+            if (map == null)
+                return null;
+            StringBuilder mapString = new StringBuilder();
+            for (Map.Entry<String, Boolean> entry : map.entrySet())
+                mapString.append(entry.getKey()).append(":").append(entry.getValue()).append(",");
+            return mapString.toString();
+        }
+    }
+
+    public static class UserLocationConverter {
+        @TypeConverter
+        public UserLocation fromString(String value) {
+            if (value == null || value.equals(""))
+                return null;
+            double lat = Double.parseDouble(value.split(" ")[0]);
+            double lang = Double.parseDouble(value.split(" ")[1]);
+            return new UserLocation(lat, lang);
+        }
+
+        @TypeConverter
+        public String asString(UserLocation warehouseUserLocation) {
+            return warehouseUserLocation == null ? "" : warehouseUserLocation.getLat() + " " + warehouseUserLocation.getLon();
+        }
     }
 }
 
